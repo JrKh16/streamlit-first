a50d73440adf186f6342b8b4eef1a5b7b2a1750a
JrKh16 <JirawatKhunngean@gmail.com>
2024-07-13 17:15:55 +0700

    update text

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 st.line_chart(chart_data)
 
 st.title("Hello P!")
+st.title("This is my first streamlit")
 
 st.write("eiei")
 st.info("info")
